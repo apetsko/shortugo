2fd94b1ec9140f8362e374bd16464a4b57523738
apetsko <apetskom@gmail.com>
2024-12-04 21:57:04 +0500

    iter5

--- internal/config/config.go
+++ internal/config/config.go
@@ -4,25 +4,35 @@ import (
 	"flag"
 	"fmt"
 	"log"
+
+	"github.com/caarlos0/env/v11"
 )
 
 type config struct {
-	Host    string
-	BaseURL string
+	Host    string `env:"BASE_URL"`
+	BaseURL string `env:"SERVER_ADDRESS"`
 }
 
 func Parse() config {
 	c := new(config)
 
+	if err := env.Parse(&c); err == nil {
+		if c.BaseURL != "" && c.Host != "" {
+			return *c
+		}
+
+	} else {
+		log.Fatal(err)
+	}
+
 	flag.StringVar(&c.Host, "a", "localhost:8080", "network address with port")
 	flag.StringVar(&c.BaseURL, "b", "http://localhost:8080", "base url address")
-
 	flag.Parse()
+
 	if c.BaseURL == "" || c.Host == "" {
 		fmt.Fprintf(flag.CommandLine.Output(), "Wrong params: baseUrl: %q, host: %q", c.BaseURL, c.Host)
 		flag.PrintDefaults()
 		log.Fatalf("invalid parameters: Host and BaseURL are required")
 	}
-	log.Println("cccccccccccccc", c)
 	return *c
 }
