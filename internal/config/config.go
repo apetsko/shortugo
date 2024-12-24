7ac9c1225de127bd107908788680f727d4784060
apetsko <apetskom@gmail.com>
2024-12-24 20:52:01 +0500

    fix env flags

--- internal/config/config.go
+++ internal/config/config.go
@@ -13,18 +13,14 @@ type Сonfig struct {
 }
 
 func Parse() (c Сonfig, err error) {
-	if err = env.Parse(&c); err != nil {
-		return
-	}
-
-	if c.BaseURL != "" && c.Host != "" && c.FileStoragePath != "" {
-		return
-	}
-
 	flag.StringVar(&c.Host, "a", "localhost:8080", "network address with port")
 	flag.StringVar(&c.BaseURL, "b", "http://localhost:8080", "base url address")
 	flag.StringVar(&c.FileStoragePath, "f", "db.json", "file storage name")
 	flag.Parse()
+
+	if err = env.Parse(&c); err != nil {
+		return
+	}
 
 	return c, nil
 }
