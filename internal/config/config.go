b7bf86a2bd4e6b5bca0842d4e08418e48d6f7be2
Apetsko <apetskom@gmail.com>
2025-01-20 22:26:53 +0500

    iter13

--- internal/config/config.go
+++ internal/config/config.go
@@ -2,24 +2,28 @@ package config
 
 import (
 	"flag"
+	"fmt"
 
 	"github.com/caarlos0/env/v11"
 )
 
-type Сonfig struct {
+type Config struct {
 	Host            string `env:"SERVER_ADDRESS"`
 	BaseURL         string `env:"BASE_URL"`
 	FileStoragePath string `env:"FILE_STORAGE_PATH"`
+	DatabaseDSN     string `env:"DATABASE_DSN"`
 }
 
-func Parse() (c Сonfig, err error) {
+func Parse() (c Config, err error) {
 	flag.StringVar(&c.Host, "a", "localhost:8080", "network address with port")
 	flag.StringVar(&c.BaseURL, "b", "http://localhost:8080", "base url address")
-	flag.StringVar(&c.FileStoragePath, "f", "db.json", "file storage name")
+	flag.StringVar(&c.FileStoragePath, "f", "db.json", "file storages name")
+	flag.StringVar(&c.DatabaseDSN, "d", "", "database DSN")
+
 	flag.Parse()
 
 	if err = env.Parse(&c); err != nil {
-		return
+		return c, fmt.Errorf("error while parse envs: %w", err)
 	}
 
 	return c, nil
